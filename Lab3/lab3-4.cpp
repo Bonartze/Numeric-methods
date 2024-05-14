6a144e7a6353cb5ec17782d39717552e2d0fdd8f
Bonartze <iokirilin@mail.ru>
2024-05-14 20:07:08 +0300

    fixed lab 4

--- Lab3/lab3-4.cpp
+++ Lab3/lab3-4.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 class Diff {
 private:
-    vector<double> x = {-1.0, 0.0, 1.0, 2.0, 3.0};
-    vector<double> f = {-0.7854, 0.0, 0.78540, 1.1071, 1.249};
+    vector<double> x = {0.0, 0.1, 0.2, 0.3, 0.4};
+    vector<double> f = {1.0, 1.1052, 1.2214, 1.3499, 1.4918};
 public:
-    double get_first_diff_second_accuracy(double val) {
+    double get_first_diff_second_accuracy_left(double val) {
         size_t i;
         for (i = 0; i < x.size() - 1; i++) {
             if (x[i] < val && x[i + 1] >= val)
@@ -18,6 +18,17 @@ public:
         return ((f[i + 1] - f[i]) / (x[i + 1] - x[i])) +
                ((((f[i + 2] - f[i + 1]) / (x[i + 2] - x[i + 1])) - (f[i + 1] - f[i]) / (x[i + 1] - x[i])) /
                 (x[i + 2] - x[i])) * (2 * val - x[i] - x[i + 1]);
+    }
+
+    double get_first_diff_second_accuracy_right(double val) {
+        size_t i;
+        for (i = 0; i < x.size() - 2; i++) {
+            if (x[i] <= val && x[i + 1] > val)
+                break;
+        }
+        return ((f[i + 2] - f[i + 1]) / (x[i + 2] - x[i + 1])) +
+               ((((f[i + 2] - f[i + 1]) / (x[i + 2] - x[i + 1])) - (f[i + 1] - f[i]) / (x[i + 1] - x[i])) /
+                (x[i + 2] - x[i])) * (2 * val - x[i + 1] - x[i + 2]);
     }
 
     double get_second_diff(double val) {
@@ -33,5 +44,16 @@ public:
 
 int main() {
     Diff df;
-    cout << df.get_first_diff_second_accuracy(1.0);
+    double val = 0.2;
+
+    cout << "Left-side first derivative with second-order accuracy at " << val << " is: "
+         << df.get_first_diff_second_accuracy_left(val) << endl;
+
+    cout << "Right-side first derivative with second-order accuracy at " << val << " is: "
+         << df.get_first_diff_second_accuracy_right(val) << endl;
+
+    cout << "Second derivative at " << val << " is: "
+         << df.get_second_diff(val) << endl;
+
+    return 0;
 }
